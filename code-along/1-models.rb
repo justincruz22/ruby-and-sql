1c8b9d534f1ff574ebc1368db42c222b6c784774
justincruz22 <justin.cruz1@kellogg.northwestern.edu>
2022-02-01 16:17:04 +0000

    lab 1

--- code-along/1-models.rb
+++ code-along/1-models.rb
@@ -10,11 +10,53 @@ Company.destroy_all
 
 # 1a. check out the schema file
 # 1b. check out the model file
+puts "There are now #{Company.all.count} companies."
 
 # 2. create new companies
+# the first way we can create new companies is we can put a hash. if we look at the schema.rb file (the makeup of the companies table), 
+# you'll see that a company has name, url, city, state, slogan, and time stamps
+# so if we want to create a new company, we need to give it those attributes
+values = {name: "Apple, Inc.", url: "https://apple.com", city: "Cupertino", state: "CA"}
+# this ^ is a hash. the reason we show a hash is that a database table is also key value pairs. records are pairs. 
+# if you think about this hash vs a row in a table, they're pretty much the same
+# ruby has is the same as database row
+
+apple = Company.new(values)
+# this ^ creates it in memory
+apple.save
+# this ^ commmits and saves to the database. this is running the insert statement for us
+puts "There are now #{Company.all.count} companies."
+
+values = {name: "Amazon.com, Inc.", url: "https://amazon.com", city: "Seattle", state: "WA"}
+amazon = Company.new(values)
+amazon.save
+puts "There are now #{Company.all.count} companies."
 
 # 3. query companies table
+puts california_company = Company.where({state: "CA"})[0]
+puts california_company.inspect
+# this ^^^ returns a list of things. An Array. A list of things is an array. 
 
 # 4. read column values from row
+puts california_company.read_attribute(:url)
+# ^^^^^ to ask for it to return a specific thing from the Company class (e.g., select URL), you use this
+# the shortcut is.... below. both return the smae value.
+puts california_company.url
 
 # 5. update attribute value
+california_company.write_attribute(:slogan, "Think different1.")
+california_company.slogan = "Think different."
+california_company.save
+
+puts california_company.inspect
+# you can write the attribute and set the value of it ^^ 
+
+new_company = Company.new
+new_company.name = "Tesla, Inc."
+new_company.url = "https://tesla.com"
+new_company.city = "Palo Alto"
+new_company.state = "CA"
+new_company.save
+
+puts new_company.inspect
+puts "There are now #{Company.all.count} companies."
--- labs/1-models.rb
+++ labs/1-models.rb
@@ -9,11 +9,61 @@ Contact.destroy_all
 # **************************
 
 # 1a. check out the schema file
+
+
 # 1b. check out the model file
 
+
 # 2. create 1-2 new contacts for each company (they can be made up)
+apple = Company.where({name: "Apple, Inc."})[0]
+apple_id = apple.id
+
+values = {first_name: "Craig",
+            last_name: "Federighi",
+            email: "craig@apple.com",
+            phone_number: "888-555-1212",
+            company_id: apple.id}
+contact = Contact.new(values)
+contact.save
+
+amazon = Company.where({name: "Amazon.com, Inc."})[0]
+amazon_id = amazon.id
+
+contact = Contact.new
+contact.first_name = "Andy"
+contact.last_name = "Jassy"
+contact.email = "andy@amazon.com"
+contact.phone_number = "999-867-5309"
+contact.company_id = "amazon_id"
+contact.save
+
+contact = Contact.new
+contact.first_name = "Tim"
+contact.last_name = "Cook"
+contact.email = "tim@apple.com"
+contact.phone_number = "123-867-5309"
+contact.company_id = "apple_id"
+contact.save
+
+tesla = Company.where({name: "Tesla, Inc."})[0]
+tesla_id = tesla.id
+
+contact = Contact.new
+contact.first_name = "Elon"
+contact.last_name = "Must"
+contact.email = "elon@tesla.com"
+contact.phone_number = "456-867-5309"
+contact.company_id = "tesla_id"
+contact.save
+
+
+
 
 # 3. write code to display how many contacts are in the database AND each contact's info (name, email), e.g.:
+puts "There are #{Contact.all.count} contacts."
+all_contacts = Contact.all
+# what class is all contacts? An array. it's a list of all contacts in the contacts table.
+
 
 # ---------------------------------
 # Contacts: 4
